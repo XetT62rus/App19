94867622bc3fd1810c6b02abef50013c75197f45
bim <bim@WKS348>
2022-03-30 14:53:44 +0300

    Добавьте файлы проекта.

---
+++ App19/Models/Lenght.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App19.Models
+{
+    static class Lenght
+    {
+        public static double LenghtRadius(double a) => Math.PI * 2 * a;
+    }
+}
---
+++ App19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using App19.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace App19.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private int number1;
+        public int Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double number3;
+        public double Number3
+        {
+            get => number3;
+            set
+            {
+                number3 = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Number3 = Lenght.LenghtRadius(Number1);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Number1 != 0 )
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
